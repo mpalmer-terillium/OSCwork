f404fcd69212a06d561b86931a970826913f69d1
mpalmer <mpalmer@terillium.com>
2016-02-25 14:11:53 -0600

    softcoding changes

--- OptyService/src/com/osc/optyservice/utility/InternalUtility.java
+++ OptyService/src/com/osc/optyservice/utility/InternalUtility.java
@@ -3,20 +3,36 @@ package com.osc.optyservice.utility;
 import com.osc.optyservice.dto.OSCIntegrationRequest;
 import com.osc.optyservice.valueobject.InternalOSCOptyValueObject;
 
+import com.osc.optyservice.valueobject.SoftCodingCredentials;
+
+import java.io.InputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+
+import static com.osc.optyservice.constants.OptyServiceConstants.*;
+
+import java.util.logging.Logger;
+
 public class InternalUtility {
+    
+    private static Logger logger = Logger.getLogger("Util");
 
 
     public static InternalOSCOptyValueObject validateRequest(OSCIntegrationRequest oscIntReq) {
         
         InternalOSCOptyValueObject internalVO = new InternalOSCOptyValueObject();
+        SoftCodingCredentials cred = securitySetup();
         
-        if(oscIntReq.getUsername() == null || oscIntReq.getPassword() == null || oscIntReq.getTransactionType() == null) {
+        if(!cred.hasCredentials() || oscIntReq.getTransactionType() == null) {
             
             internalVO.setError(true);
             
         } else {
-            internalVO.setUsername(oscIntReq.getUsername());
-            internalVO.setPassword(oscIntReq.getPassword());
+            internalVO.setUsername(cred.getUsername());
+            internalVO.setPassword(cred.getPassword());
             internalVO.setTransactionType(oscIntReq.getTransactionType());
             
             if(oscIntReq.getOptyId() != null) {
@@ -30,5 +46,39 @@ public class InternalUtility {
             }
         }
         return internalVO;
+    }
+    
+    /**
+     * public method to call the REST mock softcoding service.
+     * @return
+     */
+    public static SoftCodingCredentials securitySetup() {
+        return setUsernamePassword(new SoftCodingCredentials());
+    }
+    
+    /**
+     * this is a mock softcoding call - it will be used in JDE to get user information, currently running locally
+     * as another (here REST) service.
+     * 
+     * @param scr 
+     * @return SoftCodingCredentials
+     */
+    private static SoftCodingCredentials setUsernamePassword(SoftCodingCredentials scr) {
+        try {
+            URL url = new URL(SOFTCODING_URI);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/xml");
+            
+            JAXBContext jaxbContext = JAXBContext.newInstance(SoftCodingCredentials.class);
+            InputStream xml = connection.getInputStream();
+            
+            scr = (SoftCodingCredentials) jaxbContext.createUnmarshaller().unmarshal(xml);
+            
+            connection.disconnect();
+        } catch (Exception e) {
+            logger.severe(e.toString());
+        }
+        return scr;
     }
 }
